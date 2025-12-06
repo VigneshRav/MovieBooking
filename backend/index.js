ba2c8955e8dec9052d1e8af99337870405c5ebd0
VigneshRav <vignesh212000@gmail.com>
2025-12-06 14:47:22 +0530

    Website Icon updated

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,3 @@
-// import cors from 'cors';
-// import 'dotenv/config';
-// import express from 'express';
-// import { connectDB } from './config/db.js';
-// import path from 'path';
-// import movieRouter from './routes/movieRouter.js';
-// import userRouter from './routes/userRouter.js';
-// import bookingRouter from './routes/bookingRouter.js';
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// // Middleware
-// app.use(cors())
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // Database Connection
-// connectDB();
-
-// // Routes
-// app.use("/uploads", express.static(path.join(process.cwd(), "uploads"))); 
-// app.use("/api/auth", userRouter)
-// app.use("/api/movies", movieRouter);
-// app.use("/api/bookings", bookingRouter);
-
-
-// app.get('/', (req, res) => {
-//     res.send('API Working');
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server Started on http://localhost:${port}`);
-// });
-
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
@@ -46,15 +11,18 @@ import bookingRouter from "./routes/bookingRouter.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Database Connection
 connectDB();
 
 // STATIC FOR IMAGES
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
+// Routes
 app.use("/api/auth", userRouter);
 app.use("/api/movies", movieRouter);
 app.use("/api/bookings", bookingRouter);
